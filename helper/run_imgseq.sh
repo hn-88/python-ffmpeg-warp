8cbe93fd843a87af354f1c035893370fc5c9a8f0
hn-88 <hn-88@users.noreply.github.com>
2026-04-24 14:03:22 +0530

    Update run_imgseq.sh with documentation on the venv

--- helper/run_imgseq.sh
+++ helper/run_imgseq.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 source ~/venvs/gui_env/bin/activate
+# This virtual environment was created with
+# python3 -m venv ~/venvs/gui_env
+# followed by
+# source ~/venvs/gui_env/bin/activate
+# python -m pip install --upgrade pip
+# python -m pip install numpy scipy pillow
+# as mentioned at https://hnsws.blogspot.com/2025/12/using-py-ffmpeg-warp-on-mac.html
 cd /Users/hari/Downloads/python-ffmpeg-warp-main
 python3 imgseq_to_video.py
 
